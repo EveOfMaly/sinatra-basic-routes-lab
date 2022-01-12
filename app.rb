2cb7ec052984c4ab864e7a3b49074694de9ed7c5
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-12 10:11:59 -0500

    Completed assignment

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello World"
+    end
+
+    get '/name' do 
+        "My name is __"
+    end
+
+    get '/hometown' do 
+        "My hometown is ____"
+    end
+
+
+    get '/favorite-song' do 
+        "My favorite song is ___"
+    end
 end
